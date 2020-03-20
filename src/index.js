40326aab7eda4645b1d605d146e0fbf66f71f360
Vladimir Kormakov <cenizo.kht@gmail.com>
2020-03-20 13:23:38 +0300

    fix: add code at index.js

--- src/index.js
+++ src/index.js
@@ -1,12 +1,42 @@
 
-exports.min = function min (array) {
-  return 0;
+exports.min = function min(array) {
+  if (!Array.isArray(array)) {
+    return 0;
+  }
+  let answer = 0
+  array.forEach(element => {
+    if (answer > element) {
+      answer = element
+    }
+  });
+  return answer;
 }
 
-exports.max = function max (array) {
-  return 0;
+exports.max = function max(array) {
+  if (!Array.isArray(array)) {
+    return 0;
+  }
+  let answer = 0
+  array.forEach(element => {
+    if (answer < element) {
+      answer = element
+    }
+  });
+  return answer;
 }
 
-exports.avg = function avg (array) {
-  return 0;
+exports.avg = function avg(array) {
+  if (array === undefined) {
+    return 0;
+  }
+  if (array.length === 0) {
+    return 0;
+  }
+  if (!Array.isArray(array)) {
+    return 0;
+  }
+  let answer = array.reduce((sum, element) => {
+    return sum + element;
+  }, 0);
+  return answer / array.length;
 }
